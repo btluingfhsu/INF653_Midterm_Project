1fbe478f553fc1c5fb3ab115712f0993f3f46e85
Brian Luing <btluing@mail.fhsu.edu>
2024-03-17 16:55:32 -0500

    updated database file

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@
             $this->conn = null;
 
             try {
-                $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
+                $dsn = 'pgsql:host=' . $this->host . ';dbname=' . $this->db_name;
                 $this->conn = new PDO($dsn, $this->username, $this->password);
 
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
